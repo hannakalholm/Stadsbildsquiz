48f7b71140424499c73d2a2b5ce164ec60f76163
Hanna Kalholm <hannakalholm@gmail.com>
2018-04-13 11:47:27 +0200

    Refactor the code, delete some unnecessary classes.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,21 +1,12 @@
 package com.example.demo;
 
-import com.example.demo.domain.City;
-import com.example.demo.repository.DataRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-        // endast denna rad i main
         SpringApplication.run(DemoApplication.class, args);
-
     }
-
 }
